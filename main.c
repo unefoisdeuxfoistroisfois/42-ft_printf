f23f4d2be0f7714f12441d18bb77dbcd8b8be2bd
unefoisdeuxfoistroisfois <1fois2fois3fois@gmail.com>
2025-05-16 20:42:54 +0200

    Main fais avec comparatif entre le vrai et le faux et norminette faites pour les headers

--- main.c
+++ main.c
@@ -6,41 +6,58 @@
 /*   By: britela- <britela-@student.s19.be>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/05/11 23:55:12 by britela-          #+#    #+#             */
-/*   Updated: 2025/05/16 19:12:07 by britela-         ###   ########.fr       */
+/*   Updated: 2025/05/16 20:41:20 by britela-         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "ft_printf.h"
 #include <stdio.h>
 
-int	main()
+int	main(void)
 {
-	int	res;
-	int	x;
+	int	resultats1;
+	int	resultats2;
+	int	x = 42;
 
-	x = 42;
-	ft_printf("Bradley\n");
-	ft_printf("Charactere : %c\n", 'B');
-	ft_printf("String : %s\n", "Salut comment tu vas ?");
-	ft_printf("Adresse : %p\n", NULL);
-	ft_printf("Decimal : %d\n", -4219);
-	ft_printf("Entier : %i\n", 151);
-	ft_printf("Unsigned int : %u\n", 6543);
-	ft_printf("Hexa minuscule : %x\n", 48879);
-	ft_printf("Hexa MAJUSCULE : %X\n", 48879);
-	ft_printf("Pourcentage : %%");
+	resultats1 = ft_printf("Bradley\n");
+	resultats2 = printf("Bradley\n");
+	printf("ft_printf: %d chars | printf: %d chars\n\n", resultats1, resultats2);
 
-	printf("\n=== printf officiel ===\n");
-	printf("Bradley\n");
-	printf("Charactere : %c\n", 'B');
-	printf("String : %s\n", "Salut comment tu vas ?");
-	printf("Adresse : %p\n", NULL);
-	printf("Decimal : %d\n", -4219);
-	printf("Entier : %i\n", 151);
-	printf("Unsigned int : %u\n", 6543);
-	printf("Hexa minuscule : %x\n", 48879);
-	printf("Hexa MAJUSCULE : %X\n", 48879);
-	printf("Pourcentage : %%\n");
+	resultats1 = ft_printf("Charactere : %c\n", 'B');
+	resultats2 = printf("Charactere : %c\n", 'B');
+	printf("ft_printf: %d chars | printf: %d chars\n\n", resultats1, resultats2);
+
+	resultats1 = ft_printf("String : %s\n", "Salut comment tu vas ?");
+	resultats2 = printf("String : %s\n", "Salut comment tu vas ?");
+	printf("ft_printf: %d chars | printf: %d chars\n\n", resultats1, resultats2);
+
+	resultats1 = ft_printf("Adresse : %p\n", NULL);
+	resultats2 = printf("Adresse : %p\n", NULL);
+	printf("ft_printf: %d chars | printf: %d chars\n\n", resultats1, resultats2);
+
+	resultats1 = ft_printf("Decimal : %d\n", -4219);
+	resultats2 = printf("Decimal : %d\n", -4219);
+	printf("ft_printf: %d chars | printf: %d chars\n\n", resultats1, resultats2);
+
+	resultats1 = ft_printf("Entier : %i\n", 151);
+	resultats2 = printf("Entier : %i\n", 151);
+	printf("ft_printf: %d chars | printf: %d chars\n\n", resultats1, resultats2);
+
+	resultats1 = ft_printf("Unsigned int : %u\n", 6543);
+	resultats2 = printf("Unsigned int : %u\n", 6543);
+	printf("ft_printf: %d chars | printf: %d chars\n\n", resultats1, resultats2);
+
+	resultats1 = ft_printf("Hexa minuscule : %x\n", 48879);
+	resultats2 = printf("Hexa minuscule : %x\n", 48879);
+	printf("ft_printf: %d chars | printf: %d chars\n\n", resultats1, resultats2);
+
+	resultats1 = ft_printf("Hexa MAJUSCULE : %X\n", 48879);
+	resultats2 = printf("Hexa MAJUSCULE : %X\n", 48879);
+	printf("ft_printf: %d chars | printf: %d chars\n\n", resultats1, resultats2);
+
+	resultats1 = ft_printf("Pourcentage : %%\n");
+	resultats2 = printf("Pourcentage : %%\n");
+	printf("ft_printf: %d chars | printf: %d chars\n\n", resultats1, resultats2);
 
 	return (0);
 }
